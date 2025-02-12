433873c8fce733fe4705e4943e3edb1facf892a8
Manoj Dasari <dasarimanoj265@gmail.com>
2025-02-12 11:52:48 +0530

    update README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function HomePage() {
+"use client";
+
+
+
+export default function GoogleDrive() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
